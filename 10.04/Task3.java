e3330576883d2339e3e0776c5d3fc90216518f0f
Natali <n.serdeva@gmail.com>
2018-04-10 12:13:55 +0300

    Add files via upload

---
+++ 10.04/Task3.java
@@ -0,0 +1,48 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = in.nextInt();
+		}
+		findCommonDividers(arr);
+	}
+
+	static void findCommonDividers(int[] arr) {
+		int minElement = getMinElement(arr);
+		findCommonDividersHelper(arr, 2, minElement);
+	}
+
+	private static void findCommonDividersHelper(int[] arr, int divider, int min) {
+		if (divider <= min/divider) {
+			boolean isCommonDivider = true;
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] % divider != 0) {
+					isCommonDivider = false;
+					break;
+				}
+			}
+			if (isCommonDivider) {
+				System.out.print(divider + " ");
+			}
+			findCommonDividersHelper(arr, divider + 1, min);
+		}
+	}
+
+	static int getMinElement(int[] arr) {
+		int min = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		return min;
+	}
+
+}
