00d6787a18a11b99ad8a58531c47d0e9e3cad2a6
nserdeva <n.serdeva@gmail.com>
2018-04-10 10:37:33 +0300

    Arrays Exercise

---
+++ 10.04/Task1.java
@@ -0,0 +1,27 @@
+package tasks;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task1 {
+	
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int n = in.nextInt();
+	int[] arr = new int[n];
+	for(int i = 0; i<n; i++) {
+		int temp = in.nextInt();
+		if(temp%2==0) {
+			arr[i] = 0;
+		} else {
+			arr[i] = temp;
+		}
+	}
+	print(arr);
+	}
+
+	static void print(int[] arr) {
+		for(int i = 0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
